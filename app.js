71b5bbe6ba44f9f1213428bf226f3816785d59d9
shivamgupta-07 <81613890+shivamgupta-07@users.noreply.github.com>
2021-10-01 21:37:55 +0530

    Update app.js
    Adding the functioning of app.post() after the form is submitted and fetching data from api.openweathermap.org

--- app.js
+++ app.js
@@ -12,8 +12,27 @@ app.get("/", function(req, res){
 })
 
 app.post('/', function(req, res){
-    console.log("post request successful");
-    res.redirect('/');
+    const query = req.body.cityName;
+    const apiId = "";  //adding the API key
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiId + "&q="+ query + "&units=" + unit;
+    
+    https.get(url, function(response){
+        
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const weather = weatherData.weather[0].description;
+            const temp = weatherData.main.temp;
+            const icon = weatherData.weather[0].icon;
+            const imageUrl = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+            
+            res.setHeader("Content-Type", "text/html");
+            
+            res.write("<h3> The weather in " + query + " is : " + weather + "</h3>");
+            res.write("<img src= "+ imageUrl + " >");
+            res.write("<h1> The temperature in " + query + " is : " + temp + " degree celcius </h1>");
+            res.send();
+            console.log("Post request received");
 })
 
 app.listen(3000, function(){
