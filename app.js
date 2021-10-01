315cc4c4560ddee1d71f720abd2e44a5764c5ac2
shivamgupta777 <88576082+shivamgupta777@users.noreply.github.com>
2021-10-01 21:11:53 +0530

    Adding body-parser and app.get() in app.js

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+})
 
 app.listen(3000, function(){
     console.log("Server running at 3000 port")
 })
+
